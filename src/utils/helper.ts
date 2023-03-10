85615f0799ba67ab8c78a7b9c160dc3698fd3650
chenminggui <1728903026@qq.com>
2023-03-10 11:26:23 +0800

    feat: 计算文件字节大小-工具

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -15,3 +15,18 @@ export const formatFromData = (data: Record<string, any>) => {
  * 判空
  */
 export const isNull = (data: any): boolean => _.size(data) === 0;
+
+/**
+ * 计算文件大小
+ */
+export const byte2size = (bytes: number) => {
+  if (bytes === 0) return '0 B';
+  const k = 1024, //or 1000
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  // 精度调整
+  const precision = bytes / Math.pow(k, i) > 1000 ? 4 : 3;
+
+  return (bytes / Math.pow(k, i)).toPrecision(precision) + ' ' + sizes[i];
+};
