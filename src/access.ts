6fb0c7cedd17fab334d0f6a96151571bacb196de
陈明贵 <48546154+git-cmg@users.noreply.github.com>
2024-02-28 11:17:17 +0800

    Update access.ts

--- src/access.ts
+++ src/access.ts
@@ -3,8 +3,9 @@
  **/
 import type { API } from '@/services/API';
 
-export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
+export default function access(initialState: { currentUser?: API.CurrentUser }) {
+  const { currentUser } = initialState;
+  
   return {
     canRead: currentUser && currentUser.user_status === 1,
   };
