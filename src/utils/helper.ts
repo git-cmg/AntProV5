5961d2f1ce66db6e21aba821b16587768131422a
chenminggui <c13558889425@icloud.com>
2024-11-13 10:23:26 +0800

    feat: 移除计算文件大小，这个函数还有问题

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -10,18 +10,3 @@ export const formatFromData = (data: Record<string, any>) => {
   });
   return formData;
 };
-
-/**
- * 计算文件大小
- */
-export const byte2size = (bytes: number) => {
-  if (bytes === 0) return '0 B';
-  const k = 1024, //or 1000
-    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-    i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  // 精度调整
-  const precision = bytes / Math.pow(k, i) > 1000 ? 4 : 3;
-
-  return (bytes / Math.pow(k, i)).toPrecision(precision) + ' ' + sizes[i];
-};
